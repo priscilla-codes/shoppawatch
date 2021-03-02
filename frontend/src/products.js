9e5e5aa13da932cdf93aa36d6c04561c28322b3e
Priscilla Kalilombe <cillatx30@gmail.com>
2021-03-02 01:40:53 -0600

    Add dummy data for products

---
+++ frontend/src/products.js
@@ -0,0 +1,99 @@
+const products = [
+  {
+    _id: '1',
+    name: 'First Watch',
+    image: '/images/fossil4.jpg',
+    description:
+      'Morbi fringilla tempus tellus eu cursus. Duis in magna vehicula, efficitur lectus nec, vehicula nisi. Praesent elit justo, sodales nec mollis id, scelerisque vitae eros.',
+    price: 98.99
+  },
+  {
+    _id: '2',
+    name: 'Second Watch',
+    image: '/images/timex.jpg',
+    description:
+      'Nam sem tortor, ultricies quis pretium sit amet, aliquam eget turpis. In tristique, lorem ut ultricies vulputate, ex est condimentum neque, ut interdum neque ligula at quam. Mauris efficitur enim ut est finibus facilisis.',
+    price: 43.99
+  },
+  {
+    _id: '3',
+    name: 'Third Watch',
+    image: '/images/fossil2.jpg',
+    description:
+      'Vivamus feugiat, massa ac congue tempus, ligula est dignissim tellus, cursus tincidunt augue tellus non ex. ',
+    price: 107.99
+  },
+  {
+    _id: '4',
+    name: 'Fourth Watch',
+    image: '/images/nine-west.jpg',
+    description:
+      'Aenean quis ex libero. In sit amet lectus iaculis, consectetur sem non, venenatis justo. Morbi porttitor libero a lorem hendrerit, vitae semper nunc vehicula. Nam nec enim sit amet nibh vehicula venenatis. Morbi egestas pulvinar tortor. Sed tincidunt, nibh vel ornare pellentesque, nunc velit pellentesque lectus, eget tempus sem ligula et erat.',
+    price: 29.99
+  },
+  {
+    _id: '5',
+    name: 'Fifth Watch',
+    image: '/images/michael-kors.jpg',
+    description:
+      'Fusce dignissim quis nisi non pharetra. Maecenas a vulputate lectus. Ut quis justo a ligula posuere congue. Nam laoreet suscipit elit, rhoncus dapibus lacus auctor eget. Curabitur id vulputate magna, dignissim facilisis elit. Vestibulum eu rhoncus odio. Mauris urna eros, pharetra et lacinia nec, hendrerit sodales tortor. ',
+    price: 118.99
+  },
+  {
+    _id: '6',
+    name: 'Sixth Watch',
+    image: '/images/michael-kors2.jpg',
+    description:
+      'Nullam pulvinar urna vitae quam feugiat tincidunt. Ut euismod mollis est et faucibus. Donec lobortis gravida semper. Donec ut commodo purus. Nunc dolor ligula, pretium id hendrerit in, commodo a purus.',
+    price: 82.99
+  },
+  {
+    _id: '7',
+    name: 'Seventh Watch',
+    image: '/images/fossil7.jpg',
+    description:
+      'Etiam sed rhoncus mauris. Nunc porta risus nec sem semper vestibulum sit amet ut dui. Curabitur lobortis nisl ut purus viverra gravida. Donec ornare tristique ligula, vel ullamcorper lorem. ',
+    price: 123.99
+  },
+  {
+    _id: '8',
+    name: 'Eighth Watch',
+    image: '/images/amazon-essentials.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    price: 20.99
+  },
+  {
+    _id: '9',
+    name: 'Ninth Watch',
+    image: '/images/apple.jpg',
+    description:
+      'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    price: 272.99
+  },
+  {
+    _id: '10',
+    name: 'Tenth Watch',
+    image: '/images/fossil5.jpg',
+    description: 'Nunc sit amet laoreet lorem, non eleifend ipsum.',
+    price: 85.99
+  },
+  {
+    _id: '11',
+    name: 'Eleventh Watch',
+    image: '/images/casio_g-shock.jpg',
+    description:
+      'Pellentesque quis purus vitae mauris feugiat bibendum. Sed quam dui, dictum quis aliquet eu, porttitor tempor purus.',
+    price: 99.99
+  },
+  {
+    _id: '12',
+    name: 'Twelfth Watch',
+    image: '/images/fossil6.jpg',
+    description:
+      'Aenean diam ante, posuere sed euismod eget, accumsan ut ex. Curabitur sem libero, molestie sed feugiat dictum, commodo non urna. Fusce mi nibh, dignissim in urna quis, tincidunt faucibus lorem. Pellentesque augue nisi, pulvinar sit amet consectetur eget, sollicitudin sed ligula. Suspendisse nulla ex, laoreet a blandit vitae, laoreet id lorem. Duis nec tempus dolor. Aenean vitae bibendum justo. Etiam pulvinar, est id fermentum fringilla, purus justo facilisis est, sed pulvinar magna est eget est. Maecenas vestibulum non nibh et porta. Vivamus vehicula metus est, nec semper risus faucibus venenatis.',
+    price: 79.99
+  }
+];
+
+export default products;
