a81c1a5d8d4c61d8329fba3d7486ac59a6c6b1a5
Priscilla Kalilombe <cillatx30@gmail.com>
2021-04-23 21:57:30 -0500

    Update routes

--- backend-api/config/routes.rb
+++ backend-api/config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :products, only: [:index, :show]
+      resources :carts do
+        collection do
+          get "get_cart"
+          post "add_item"
+        end
+      end
     end
   end
 end
