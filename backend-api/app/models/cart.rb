4d0909b38708ee8d617922c1fba37ec42ce88c51
Priscilla Kalilombe <cillatx30@gmail.com>
2021-04-23 21:58:46 -0500

    Add total_items method to Cart model

--- backend-api/app/models/cart.rb
+++ backend-api/app/models/cart.rb
@@ -1,4 +1,12 @@
 class Cart < ApplicationRecord
   belongs_to :user
   has_many    :cart_items
+
+  def total_items
+    item_count = 0  
+    cart_items.each do |item|
+      item_count += item.quantity
+    end
+    item_count
+  end
 end
