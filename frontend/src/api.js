7327bf5da8437eccae23ae05c283c0390750e46a
Priscilla Kalilombe <cillatx30@gmail.com>
2021-04-24 01:33:56 -0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,12 +2,16 @@ import axios from 'axios';
 
 const baseUrl = 'http://localhost:8000';
 const products = 'api/v1/products/';
+const addItem = 'api/v1/carts/add_item';
+const cart = 'api/v1/carts/get_cart';
 
 axios.defaults.baseURL = baseUrl;
 
 const api = {
   baseUrl,
-  products
+  products,
+  addItem,
+  cart
 };
 
 export default api;
