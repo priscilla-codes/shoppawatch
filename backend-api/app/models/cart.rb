b5fb13aa3255b0aaca12b3f56eb7a91ca45cac32
Priscilla Kalilombe <cillatx30@gmail.com>
2021-04-24 00:46:16 -0500

    Add get_cart and add_item methods

--- backend-api/app/models/cart.rb
+++ backend-api/app/models/cart.rb
@@ -2,6 +2,20 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many    :cart_items
 
+  def add_product(product, quantity)
+    current_item = cart_items.find_by(product_id: product.id)
+    if current_item
+      if quantity > 1
+        current_item.quantity += quantity
+      else
+        current_item.quantity += 1
+      end
+    else 
+      current_item = cart_items.build(product_id: product.id, quantity: quantity)
+    end
+    current_item
+  end
+
   def total_items
     item_count = 0  
     cart_items.each do |item|
