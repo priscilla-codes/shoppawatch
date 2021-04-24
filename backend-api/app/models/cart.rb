ff306a063cbeebf7956d87506e89bb348a53bfe0
Priscilla Kalilombe <cillatx30@gmail.com>
2021-04-24 01:31:01 -0500

    Remove user_id from carts

--- backend-api/app/models/cart.rb
+++ backend-api/app/models/cart.rb
@@ -1,6 +1,5 @@
 class Cart < ApplicationRecord
-  belongs_to :user
-  has_many    :cart_items
+  has_many  :cart_items
 
   def add_product(product, quantity)
     current_item = cart_items.find_by(product_id: product.id)
