06ae34befe47c8217722bae91a1dd8c288c5b31b
Priscilla Kalilombe <cillatx30@gmail.com>
2021-04-27 23:52:06 -0500

    Create CartPage component

---
+++ frontend/src/pages/CartPage.js
@@ -0,0 +1,11 @@
+import MainContent from '../components/MainContent';
+
+const CartPage = () => {
+  return (
+    <>
+      <MainContent page="cart"></MainContent>
+    </>
+  );
+};
+
+export default CartPage;
