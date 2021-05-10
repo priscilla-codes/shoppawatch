37aaf2b349460a1b9baaba20e1e6c8dae8f3fc80
Priscilla Kalilombe <cillatx30@gmail.com>
2021-05-10 00:42:09 -0500

    Add has_secure_pasword to User

--- backend-api/app/models/user.rb
+++ backend-api/app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :orders
+  has_secure_password
+
+  validates_presence_of :email
 end
--- backend-api/db/schema.rb
+++ backend-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_031348) do
+ActiveRecord::Schema.define(version: 2021_05_10_003414) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2021_04_24_031348) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
   end
