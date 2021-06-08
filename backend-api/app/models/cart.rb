b86bdfc82fcd98b95c9a2a67d6acc320b5d5c45a
Priscilla Kalilombe <cillatx30@gmail.com>
2021-06-08 18:49:34 -0500

    Add user_id to Cart

--- backend-api/app/models/cart.rb
+++ backend-api/app/models/cart.rb
@@ -1,5 +1,6 @@
 class Cart < ApplicationRecord
   has_many  :cart_items
+  belongs_to :user, optional: true
 
   def add_product(product, quantity)
     current_item = cart_items.find_by(product_id: product.id)
--- backend-api/app/models/user.rb
+++ backend-api/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :orders
+  has_one :cart
   has_secure_password
 
   validates_presence_of :email
