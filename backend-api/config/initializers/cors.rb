e43d10445780a694f6b5b155f3075f9197303e97
Priscilla Kalilombe <cillatx30@gmail.com>
2021-07-10 14:54:31 -0500

    Update cors.rb for production

--- backend-api/config/initializers/cors.rb
+++ backend-api/config/initializers/cors.rb
@@ -14,4 +14,13 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       methods: [:get, :post, :put, :patch, :delete, :options, :head],
       credentials: true
   end
+
+  allow do
+    origins 'https://shoppawatch-frontend.herokuapp.com/'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
 end
