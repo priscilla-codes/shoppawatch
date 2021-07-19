4dc98db6a9ba6d2983cb1feb9f76d5904a8cefad
Priscilla Kalilombe <cillatx30@gmail.com>
2021-07-19 03:10:15 -0500

    Update main_image url in product_serializer.rb

--- backend-api/app/serializers/product_serializer.rb
+++ backend-api/app/serializers/product_serializer.rb
@@ -4,6 +4,6 @@ class ProductSerializer < ActiveModel::Serializer
   attributes :id, :name, :brand, :description, :price, :main_image
 
   def main_image
-    rails_blob_path(object.main_image, only_path: true) if object.main_image.attached?
+     object.main_image.url if object.main_image.attached?
   end
 end
