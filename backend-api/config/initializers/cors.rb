7066075b2cbfdf3cf848b9f32db367e28e2f9b65
Priscilla Kalilombe <cillatx30@gmail.com>
2021-07-25 02:07:01 -0500

    Set forgery_protection_origin_check to false in cors.rb

--- backend-api/config/initializers/cors.rb
+++ backend-api/config/initializers/cors.rb
@@ -24,3 +24,5 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       credentials: true
   end
 end
+
+Rails.application.config.action_controller.forgery_protection_origin_check = false
