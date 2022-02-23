643820135dfd3b58c5c36ee21ca1271f8ecb9658
Priscilla Kalilombe <priscilla@afroic.com>
2022-02-23 00:19:44 -0600

    Update 'create' action in Registrations controller

--- backend-api/app/controllers/api/v1/registrations_controller.rb
+++ backend-api/app/controllers/api/v1/registrations_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::RegistrationsController < ApplicationController
   
   def create 
     user = User.create!(
+      name: params['user']['name'],
       email: params['user']['email'],
       password: params['user']['password']
     )
