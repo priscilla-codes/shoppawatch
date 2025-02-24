8c45bddce7b07666dfd3a43942782cecf867d810
Priscilla <priscilla@afroic.com>
2025-02-23 18:25:55 -0600

    Fix image URL handling in product serializer

--- backend-api/app/serializers/product_serializer.rb
+++ backend-api/app/serializers/product_serializer.rb
@@ -1,15 +1,15 @@
 class ProductSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :brand, :description, :price, :main_image_url
+  attributes :id, :name, :brand, :description, :price, :main_image
 
-  def main_image_url
+  def main_image
     return unless object.main_image.attached?
     
     # Return a static URL in test environment to avoid URL generation issues
     return "http://example.com/test-image.jpg" if Rails.env.test?
     
     # Generate a URL for the attached image in non-test environments
-    rails_blob_url(object.main_image)
+    object.main_image.url
   end
 end
