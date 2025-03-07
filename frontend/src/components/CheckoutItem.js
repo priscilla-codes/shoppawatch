c009c7296cf14fe9a64ab91943db0509c5f09573
Priscilla <priscilla@afroic.com>
2025-03-07 01:48:05 -0600

    Add product images to checkout page

--- frontend/src/components/CheckoutItem.js
+++ frontend/src/components/CheckoutItem.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const CheckoutItem = ({ item, usCurrency }) => {
   return (
-    <div class="watch-order-details">
-      <div class="watch-order-details-left">
-        <div class="watch-name__checkout">{item.name}</div>
-        <div class="quantity__checkout">
-          <span class="quantity-text"></span>Quantity:
-          <span class="quantity-count__checkout">{item.quantity}</span>
+    <div className="watch-order-details">
+      <div className="watch-order-details-left">
+        <Link to={`/products/${item.product_id}`}>
+          <img 
+            src={item.main_image} 
+            alt={item.name}
+            className="watch-image__checkout"
+          />
+        </Link>
+        <div className="watch-details__checkout">
+          <div className="watch-name__checkout">
+            <Link to={`/products/${item.product_id}`} className="product-link">
+              {item.name}
+            </Link>
+          </div>
+          <div className="quantity__checkout">
+            <span className="quantity-text">Quantity:</span>
+            <span className="quantity-count__checkout">{item.quantity}</span>
+          </div>
         </div>
       </div>
-      <div class="price__checkout">
-        <span class="watch-price__checkout">
+      <div className="price__checkout">
+        <span className="watch-price__checkout">
           {usCurrency.format(item.price)}{' '}
         </span>
       </div>
